2734c3a46a85f939519a423e0673eadfd6b2ef3b
Hirunya Dimanthi <166610146+Hirunya003@users.noreply.github.com>
2026-03-20 09:00:00 +0530

    Adding Resources files

---
+++ BACKEND/src/main/java/com/campusnex/hub/repository/CommentRepository.java
@@ -0,0 +1,12 @@
+package com.campusnex.hub.repository;
+
+import com.campusnex.hub.model.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findByTicketIdOrderByCreatedAtAsc(Long ticketId);
+}
